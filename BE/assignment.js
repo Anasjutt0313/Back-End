d987b9d0da0636901656fbad907072312ca4b9ea
Anasjutt0313 <hafizanas0313@gmail.com>
2024-11-02 10:48:17 +0500

    update-assignment1
    using res status hereeee....

--- BE/assignment.js
+++ BE/assignment.js
@@ -1,8 +1,8 @@
 const express = require('express');
-
+const bodyParser = require('body-parser')
 const app = express();
 const PORT = 3000;
-
+app.use(bodyParser.json());
 app.listen(PORT, () => {
     console.log("Server is running on http://localhost:");
 });
@@ -23,25 +23,25 @@ const books= [{
 
     
 app.get('/login', (req, res) => {
-    res.send('Welcome to the Book API');
+    res.status(200).send('Welcome to the Book API');
 });
 
 app.get('/list', (req, res) => {
-    res.send(books);
+    res.status(200).send(books);
 });
 
 app.post('/add', (req, res) => {
     
-    res.send('Book added successfully');
+    res.status(200).send('Book added successfully');
 });
 
  
 app.put('/update', (req, res) => {
-    res.send('update the book');
+    res.status(200).send('update the book');
 });
 
 app.delete('/update', (req, res) => {
-    res.send('delete the book');
+    res.status(200).send('delete the book');
 });
 
 
